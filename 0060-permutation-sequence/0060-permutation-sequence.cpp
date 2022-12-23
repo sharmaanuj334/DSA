ac17d51201e84d4c710ea85030c568c4d92c38d8
Anuj Sharma <95574414+yoco-a11@users.noreply.github.com>
2022-12-23 17:35:29 +0530

    Time: 4 ms (51.57%), Space: 6 MB (50.71%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,26 +1,21 @@
 class Solution {
-public: 
-    void pos(string &ans, set<int> &taken, int k, int n, int fac, int cases){
-        for (int ind=1; ind<=n; ind++){
-            if (taken.count(ind)) continue;
-            cases += fac;
-            if (k <= cases){
-                ans += ind + '0';
-                cases -= fac;
-                if (ans.length() == n) return;
-                fac /= n - ans.length();
-                taken.insert(ind);
-                pos(ans, taken, k, n, fac, cases);
-                break;
-            }
-        }
-    }
 public:
     string getPermutation(int n, int k) {
-        set<int> taken;
+        vector<int> s;
+        int fac = 1; for (int i=1; i<=n; i++){
+            fac *= i;
+            s.push_back(i);
+        }
+        fac /= n;
         string ans = "";
-        int fac = 1; for (int i=1; i<n; i++) fac *= i;
-        pos(ans, taken, k, n, fac, 0);
+        k--;
+        while(true){
+            ans += '0' + s[k / fac];
+            if (ans.length() == n) break;
+            s.erase(s.begin() + k/fac);
+            k %= fac;
+            fac /= s.size();
+        }
         return ans;
     }
 };
