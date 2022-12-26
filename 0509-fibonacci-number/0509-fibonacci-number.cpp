fc16bd8277253ed2742200121bf315e5db0990d4
Anuj Sharma <95574414+yoco-a11@users.noreply.github.com>
2022-12-26 16:26:08 +0530

    Time: 3 ms (65.66%), Space: 6 MB (17.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,9 +2,13 @@ class Solution {
 public:
     int fib(int n) {
         if (n == 0) return 0;
-        int dp[n+1];
-        dp[0] = 0; dp[1] = 1;
-        for (int i=2; i<=n; i++) dp[i] = dp[i-1] + dp[i-2];
-        return dp[n];
+        int prev = 0;
+        int fib = 1;
+        for (int i=2; i<=n; i++){
+            int tmp = fib;
+            fib += prev;
+            prev = tmp;
+        }
+        return fib;
     }
 };
