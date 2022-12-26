6d2a77c8b89dbaba367a068ef11035c7525699a1
Anuj Sharma <95574414+yoco-a11@users.noreply.github.com>
2022-12-26 16:32:05 +0530

    Time: 3 ms (35.03%), Space: 5.9 MB (66.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,11 @@
 class Solution {
-public: 
-    int dp(int i, int n, int a[]){
-        if (i == n) return 1;
-        if (i > n) return 0;
-        if (a[i] != -1) return a[i];
-        a[i] = dp(i+1, n, a) + dp(i+2, n, a);
-        return a[i];
-    }
 public:
     int climbStairs(int n) {
         int a[n+1];
-        memset(a, -1, sizeof(a));
-        return dp(0, n, a);
+        a[n] = 1; a[n-1] = 1;
+        for (int i=n-2; i>=0; i--){
+            a[i] = a[i+1] + a[i+2];
+        }
+        return a[0];
     }
 };
