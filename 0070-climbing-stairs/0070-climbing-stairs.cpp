6aa9c6e8b3ea19cc3b7df14565374d8f58579bb5
Anuj Sharma <95574414+yoco-a11@users.noreply.github.com>
2022-12-26 16:34:14 +0530

    Time: 3 ms (35.03%), Space: 5.9 MB (66.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int a[n+1];
-        a[n] = 1; a[n-1] = 1;
+        int prev = 1;
+        int now = 1;
         for (int i=n-2; i>=0; i--){
-            a[i] = a[i+1] + a[i+2];
+            int tmp = now;
+            now = now + prev;
+            prev = tmp;
         }
-        return a[0];
+        return now;
     }
 };
