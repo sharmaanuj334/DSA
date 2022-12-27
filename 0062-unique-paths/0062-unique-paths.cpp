0be1708f49a4e7e851def9b57bc1c7f72c1cb5be
Anuj Sharma <95574414+yoco-a11@users.noreply.github.com>
2022-12-28 02:06:17 +0530

    Time: 5 ms (19.18%), Space: 6.5 MB (30.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:  
-int sol(int m, int n, vector<vector<int>> &dp){
-    if (n == 0 && m == 0) return 1;
-    if (n < 0 || m < 0) return 0;
-    int left = 0, top = 0;
-    if (n >= 1){
-        dp[m][n-1] = (dp[m][n-1] != -1) ? dp[m][n-1] : sol(m, n-1, dp);
-        left = dp[m][n-1];
-    }
-    if (m >= 1){
-        dp[m-1][n] = (dp[m-1][n] != -1) ? dp[m-1][n] : sol(m-1, n, dp);
-        top = dp[m-1][n];
-    }
-    return left+top;
-}
-public:
 int uniquePaths(int m, int n){
-    vector<vector<int>> dp(m, vector<int> (n, -1));
-    return sol(m-1, n-1, dp);
+    vector<vector<int>> dp(m, vector<int> (n, 0));
+    dp[0][0] = 1;
+    for (int i=0; i<m; i++){
+        for (int j=0; j<n; j++){
+            if (i-1 >= 0)
+            dp[i][j] += dp[i-1][j];
+            if (j-1 >= 0)
+            dp[i][j] += dp[i][j-1];
+        }
+    }
+    return dp[m-1][n-1];
 }
 };
