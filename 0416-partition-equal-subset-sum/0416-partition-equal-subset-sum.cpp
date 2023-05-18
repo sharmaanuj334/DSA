3cf2abc24b82831e4bb56759708bcef843c3868c
Anuj Sharma <95574414+yoco-a11@users.noreply.github.com>
2023-05-19 02:27:30 +0530

    Time: 1143 ms (5.01%), Space: 16.1 MB (59.21%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,18 +1,21 @@
 class Solution {
-private:
-    bool rec(int sum1, int sum2, int i, vector<int> &nums, vector<vector<int>> &dp){
-        if (sum1 == sum2) return true;
-        if (i == nums.size()) return false;
-        if (dp[i][sum1] != -1) return dp[i][sum1];
-        if (nums[i] <= sum1){
-            return dp[i][sum1] = rec(sum1-nums[i], sum2+nums[i], i+1, nums, dp) | rec(sum1, sum2, i+1, nums, dp);
-        } 
-        return dp[i][sum1] = rec(sum1, sum2, i+1, nums, dp);
-    }
 public:
     bool canPartition(vector<int>& nums){
+        int n = nums.size();
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        vector<vector<int>> dp(nums.size(), vector<int> (sum+1, -1));
-        return rec(sum, 0, 0, nums, dp);
+        if (sum % 2) return false;
+        vector<vector<bool>> dp(nums.size(), vector<bool> (sum+1, 0));
+        for (int i=0; i<n; i++) dp[i][0] = 1;
+        dp[0][nums[0]] = 1;
+        for (int i=1; i<n; i++){
+            for (int j=0; j<=sum; j++){
+                if (nums[i] <= j){
+                    dp[i][j] = dp[i-1][j] | dp[i-1][j-nums[i]];
+                } else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[nums.size()-1][sum/2];
     }
 };
